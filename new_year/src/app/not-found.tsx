8e1c8e82c79bc047e589cc06b6770447867ceac0
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2025-01-13 20:07:08 +1200

    feat: add not-found page

---
+++ new_year/src/app/not-found.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import notFoundImage from '@/images/notFound.png';
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <main className="text-center">
+        <h1 className="text-5xl font-bold text-gray-800">Oops, the dish isn’t ready yet!</h1>
+        <p className="text-lg text-gray-600 mt-4">
+          Looks like you’ve checked in too early — this recipe is still cooking.
+          Head back to the homepage to find something else delicious!
+        </p>
+        <Image
+          src={notFoundImage}
+          alt="Empty plate"
+          className="mx-auto mt-6"
+          width={256}
+          height={256}
+        />
+        <a
+          href="/"
+          className="mt-6 inline-block bg-[#333333] text-white px-6 py-3 rounded-lg hover:bg-gray-800 transition"
+        >
+          Back to Recipes
+        </a>
+      </main>
+    </div>
+  );
+}
