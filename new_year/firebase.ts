37f5e6c808f0462fc6903606ed2f94722f539c15
antonina220590 <tonya2290@gmail.com>
2025-01-20 12:35:11 +0300

    feat: add firebase config file

---
+++ new_year/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyC9M8CFzY9OTPoofyp-uQy2F52OLMM8C9c',
+  authDomain: 'culinary-book-app.firebaseapp.com',
+  projectId: 'culinary-book-app',
+  storageBucket: 'culinary-book-app.firebasestorage.app',
+  messagingSenderId: '1024130650117',
+  appId: '1:1024130650117:web:2163167bedb357895ccc6c',
+  measurementId: 'G-4VZTGE4ZZK',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
