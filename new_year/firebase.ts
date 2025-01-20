143be29793007c2edd4a759875a3db25f50c85ba
antonina220590 <tonya2290@gmail.com>
2025-01-20 19:39:52 +0300

    feat: test data fetching from firestore database

--- new_year/firebase.ts
+++ new_year/firebase.ts
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyC9M8CFzY9OTPoofyp-uQy2F52OLMM8C9c',
   authDomain: 'culinary-book-app.firebaseapp.com',
@@ -15,5 +11,5 @@ const firebaseConfig = {
   measurementId: 'G-4VZTGE4ZZK',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
--- new_year/src/app/page.tsx
+++ new_year/src/app/page.tsx
@@ -1,4 +1,31 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { db } from '../../firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+
 export default function Home() {
+  const [recipe, setRecipe] = useState([{}]);
+  const recipeCollection = collection(
+    db,
+    'EASY',
+    '30 minutes or less',
+    'recipes'
+  );
+
+  useEffect(() => {
+    const getRecipesList = async () => {
+      const data = await getDocs(recipeCollection);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setRecipe(filteredData);
+    };
+    getRecipesList();
+  }, []);
+  console.log(recipe);
+
   return (
     <div>
       <main>Happy new year!</main>
